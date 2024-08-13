c945327cd67eb6d1403b662d3244874184ebe5be
leonas5555 <ivan.stankev@gmail.com>
2024-08-13 14:04:50 +0300

    feat: add terraform run for eventarc storage trigger

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  type        = string
+  description = "Google Cloud project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "Google Cloud region"
+}
+
+variable "bucket" {
+  type        = string
+  description = "Google Cloud bucket name"
+}
+
+variable "name" {
+  type        = string
+  description = "Google Cloud sa name"
+}
