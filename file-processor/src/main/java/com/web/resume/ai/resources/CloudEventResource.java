0b8cd6e93fa684bfb306ba66f0a61c22a5fd7796
leonas5555 <ivan.stankev@gmail.com>
2024-08-15 13:13:56 +0300

    feat: enable tika parser

--- file-processor/src/main/java/com/web/resume/ai/resources/CloudEventResource.java
+++ file-processor/src/main/java/com/web/resume/ai/resources/CloudEventResource.java
@@ -3,26 +3,28 @@ package com.web.resume.ai.resources;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.web.resume.ai.interfaces.StorageService;
+import io.quarkus.tika.TikaParser;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.apache.tika.Tika;
+import org.jboss.logging.Logger;
+import org.jboss.logging.Logger.Level;
+
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Path("/ce")
 public class CloudEventResource {
 
     @Inject
     StorageService storageService;
+    @Inject
+    TikaParser parser;
 
-    private final Tika tika = new Tika();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     private static final Logger LOGGER = Logger.getLogger(CloudEventResource.class.getName());
@@ -32,6 +34,7 @@ public class CloudEventResource {
     public Response receiveMessage(String cloudEventJson) {
 
         LOGGER.info("Received CloudEvent: " + cloudEventJson);
+        LOGGER.info("StorageService is " + (storageService == null ? "null" : "initialized"));
 
         try {
             JsonNode rootNode = objectMapper.readTree(cloudEventJson);
@@ -40,23 +43,23 @@ public class CloudEventResource {
             JsonNode nameNode = rootNode.get("name");
 
             if (bucketNode == null || nameNode == null) {
-                LOGGER.log(Level.SEVERE, "Missing required fields in the JSON: bucket or name");
+                LOGGER.log(Level.ERROR, "Missing required fields in the JSON: bucket or name");
                 return Response.status(Response.Status.BAD_REQUEST).entity("Missing required fields: bucket or name").build();
             }
 
             String bucketName = bucketNode.asText();
             String fileName = nameNode.asText();
 
-            /*if (bucketName.isEmpty() || fileName.isEmpty()) {
-                LOGGER.log(Level.SEVERE, "Requered fields are empty");
+            if (bucketName.isEmpty() || fileName.isEmpty()) {
+                LOGGER.log(Level.ERROR, "Requered fields are empty");
                 return Response.status(Response.Status.BAD_REQUEST).entity("Missing required fields: bucket or name").build();
-            }*/
+            }
 
             if (fileName.endsWith(".pdf")) {
                 processPdfFile(bucketName, fileName);
             }
         } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error processing a file", e);
+            LOGGER.log(Level.ERROR, "Error processing a file", e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error in processing").build();
         }
 
@@ -66,11 +69,13 @@ public class CloudEventResource {
         try {
             byte[] fileContent = storageService.downloadFile(bucketName, fileName);
             try (InputStream inputStream = new ByteArrayInputStream(fileContent)) {
-                String text = tika.parseToString(inputStream);
-                System.out.println("Extracted text: " + text);
+
+                String text = parser.getText(inputStream);
+
+                LOGGER.info("Extracted text: " + text);
             }
         } catch (Exception e) {
-            //LOGGER.log(Level.SEVERE, "Error processing pdf file", e);
+            LOGGER.log(Level.ERROR, "Error processing pdf file", e);
             throw new RuntimeException(e);
         }
     }
