6b1530c266e80e9b076d57ad6f3212051000949d
leonas5555 <ivan.stankev@gmail.com>
2024-08-26 14:28:16 +0300

    chore: drop unnecessary method

--- file-processor/src/main/java/com/web/resume/ai/resources/CloudEventResource.java
+++ file-processor/src/main/java/com/web/resume/ai/resources/CloudEventResource.java
@@ -10,7 +10,6 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.jboss.logging.Logger;
@@ -18,8 +17,6 @@ import org.jboss.logging.Logger;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.time.Duration;
-import java.time.Instant;
 
 @Path("/ce")
 public class CloudEventResource {
@@ -31,25 +28,7 @@ public class CloudEventResource {
     @Inject
     PdfParserUtil pdfParserUtil;
 
-
     private static final Logger LOGGER = Logger.getLogger(CloudEventResource.class.getName());
-
-
-    @POST
-    @Path("/text")
-    @Consumes({ "application/pdf", "application/vnd.oasis.opendocument.text" })
-    @Produces(MediaType.TEXT_PLAIN)
-    public String extractText(InputStream stream) {
-        Instant start = Instant.now();
-
-        String text = parser.getText(stream);
-
-        Instant finish = Instant.now();
-
-        LOGGER.info(Duration.between(start, finish).toMillis() + " mls have passed");
-
-        return text;
-    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
